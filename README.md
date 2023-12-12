1b7a96769d5eea537ffbc7140e15425a0b77d047
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2023-12-11 19:44:27 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Discord Unicode Emojis
+
+This is a [BetterDiscord](https://betterdiscord.app/) plugin that automatically escapes emojis that you type in chat, turning them into their unicode equivalents.
+
+### No plugin:
+
+![Without plugin](/screenshots/normalEmoji.png)
+
+### With plugin:
+
+![With plugin](/screenshots/unicodeEmoji.png)
+
+## Performance
+
+If you, for some reason, paste in a lot of emojis at once, the plugin will slowly escape them one by one. This is to prevent discord from freezing, although it will still lag pretty bad. Even without this plugin, discord really struggles when presented with a lot of emojis at once.
---
+++ UnicodeEmojis.plugin.js
@@ -0,0 +1,81 @@
+/**
+ * @name UnicodeEmojis
+ * @description Replaces discord emojis that you send with their unicode equivalent
+ * @author TheLazySquid
+ * @version 0.1.0
+ * @authorId 619261917352951815
+ * @source https://github.com/TheLazySquid/DiscordUnicodeEmojis
+ */
+
+let editor;
+let waitingToUpdate = false;
+
+function onChange() {
+    // count backwards to minimize lag from shuffling around nodes
+    for(let lineIndex = editor.children.length - 1; lineIndex >= 0; lineIndex--) {
+        let line = editor.children[lineIndex];
+        for(let i = line.children.length - 1; i >= 0; i--) {
+            let child = line.children[i];
+            if (child.type != 'emoji') continue;
+            let replacement = child.emoji.surrogate;
+            if (!replacement) continue;
+            
+            // remove the emoji
+            editor.apply({ type: 'remove_node', path: [lineIndex, i] });
+    
+            let lastNode = line.children[i - 1];
+            // insert the replacement
+            editor.apply({ type: 'insert_text', path: [lineIndex, i - 1], offset: lastNode.text.length, text: `\\${replacement}` });
+
+            // if the user copy-pastes in a large amount of emojis, discord will freeze/crash otherwise
+            waitingToUpdate = true;
+            requestAnimationFrame(onChange)
+            return;
+        }
+    }
+    
+    waitingToUpdate = false;
+}
+
+let changeObserver = new MutationObserver(() => {
+    if(waitingToUpdate) return;
+    onChange();
+});
+
+function onAreaFound(textArea) {
+    // this code adapted from https://github.com/rauenzi/BDPluginLibrary/blob/master/src/modules/reacttools.js
+    let reactInstance = textArea[Object.keys(textArea).find((key) => key.startsWith("__reactInternalInstance") || key.startsWith("__reactFiber"))]
+    editor = reactInstance?.return?.return?.stateNode?.ref?.current?.getSlateEditor()
+    if(!editor) return;
+
+    changeObserver.observe(textArea, {childList: true, subtree: true})
+}
+
+const textareaSelector = ".textArea__74543"
+let textareaObserver = new MutationObserver((mutations) => {
+    for(let mutation of mutations) {
+        for(let node of mutation.addedNodes) {
+            let textarea = node.matches?.(textareaSelector) ? node : node.querySelector?.(textareaSelector)
+            if(!textarea) continue;
+
+            onAreaFound(textarea)
+        }
+    }
+})
+
+module.exports = class UnicodeEmojis {
+    start() {
+        let textarea = document.querySelector(textareaSelector)
+        if(textarea) onAreaFound(textarea)
+
+        textareaObserver.observe(document.body, {
+            childList: true,
+            subtree: true
+        })
+    }
+
+    stop() {
+        textareaObserver.disconnect()
+        changeObserver.disconnect()
+    }
+};
